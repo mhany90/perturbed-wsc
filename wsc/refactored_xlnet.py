abb5d8a60d06adf1a8fc0a8498853ff873821acf
mhany90 <mostafahany56@gmail.com>
2019-09-21 17:24:44 +0200

    shorter xlnet padding

--- wsc/refactored_xlnet.py
+++ wsc/refactored_xlnet.py
@@ -21,13 +21,7 @@ wsc_datapoints = pd.read_csv(path_to_wsc, sep='\t')
 PADDING_TEXT = """ In 1991, the remains of Russian Tsar Nicholas II and his family
 (except for Alexei and Maria) are discovered.
 The voice of Nicholas's young son, Tsarevich Alexei Nikolaevich, narrates the
-remainder of the story. 1883 Western Siberia,
-a young Grigori Rasputin is asked by his father and a group of men to perform magic.
-Rasputin has a vision and denounces one of the men as a horse thief. Although his
-father initially slaps him for making such an accusation, Rasputin watches as the
-man is chased outside and beaten. Twenty years later, Rasputin sees a vision of
-the Virgin Mary, prompting him to become a priest. Rasputin quickly becomes famous,
-with people, even a bishop, begging for his blessing. <eod> </s> <eos>"""
+remainder of the story. <eod> </s> <eos>"""
 
 def find_keyword(tokens, text):
     result = []
